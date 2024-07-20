0f6d311a8df2fcc16f2460c6da71f86c84fbf8e3
Huan Nguyen <nguyenvanhuan243@gmail.com>
2024-07-20 10:57:18 +0700

    swagger api

--- server.js
+++ server.js
@@ -5,42 +5,35 @@ import dotenv from 'dotenv';
 import connect from './database/conn.js';
 import userRouter from './router/user.js';
 import postRouter from './router/post.js';
-import commentRouter from './router/comment.js';
-
+import swaggerDocs from './swagger.js'
 
 dotenv.config()
 const app = express();
-
 /** middlewares */
 app.use(express.json());
 app.use(cors());
 app.use(morgan('tiny'));
 app.disable('x-powered-by'); // less hackers know about our stack
 
-
 const port = process.env.PORT || 8080;
-
 /** HTTP GET Request */
 app.get('/', (req, res) => {
     res.status(201).json("Home GET Request");
 });
 
-
 /** api routes */
 app.use('/api/user', userRouter)
 app.use('/api/post', postRouter)
-app.use('/api/comment', commentRouter)
-
 /** start server only when we have valid connection */
 connect().then(() => {
     try {
         app.listen(port, () => {
             console.log(`Server connected to http://localhost:${port}`);
         })
+        swaggerDocs(app, port)
     } catch (error) {
         console.log('Cannot connect to the server')
     }
 }).catch(error => {
     console.log("Invalid database connection...!");
 })
-
